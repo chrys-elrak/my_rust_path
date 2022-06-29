e624bfb2a02e560d2bfe3b5c13b0c716c6c5eef4
Chrys Rakotonimanana <chrysrakk@gmail.com>
2022-06-29 20:53:16 +0300

    fix type to f32

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod second_degree;
 
 fn main() {
-    second_degree::second_degree();
+    second_degree::main();
 }
--- src/second_degree.rs
+++ src/second_degree.rs
@@ -1,32 +1,40 @@
 use std::io;
 
-fn input_i32(prompt: &str) -> i32 {
-    let v = &mut String::new();
-    println!("{}", prompt);
-    io::stdin()
-        .read_line(v)
-        .expect("Failed to read use&r input");
-    let a: i32 = v.trim().parse().unwrap_or(0);
-    return a;
+fn input_i32(prompt: &str) -> f32 {
+    let mut text = String::new();
+    loop {
+        println!("{}", prompt);
+        io::stdin()
+        .read_line(&mut text)
+        .expect("Failed to read user input");
+        let text: f32  = match text.trim().parse() {
+            Ok(num) => num,
+            Err(x) => {
+                print!("{}", x);
+                continue
+            }
+        };
+        return text;
+    }
 }
 
-pub fn second_degree() {
+pub fn main() {
     println!("(a)x^2 + (b)x + (c)");
     let a = input_i32("Please give me a: ");
     let b = input_i32("Please give me b: ");
     let c = input_i32("Please give me c: ");
     println!("{}x^2 + {}x + {}", a, b, c);
 
-    let delta = b.pow(2) - 4 * a * c;
-    if delta == 0 {
-        let x0 = -b / 2 * a;
+    let delta = b.powf(2_f32) - 4_f32 * a * c;
+    if delta == 0_f32 {
+        let x0 = -b / 2_f32 * a;
         print!("Only one solution for it ! \n x0 = {}", x0);
-    } else if delta > 0 {
+    } else if delta > 0_f32 {
         print!("There is two solutions !");
-        let x1 = (-b as f32) - (delta as f32).sqrt() / 2.0 * (a as f32);
-        let x2 = (-b as f32) + (delta as f32).sqrt() / 2.0 * (a as f32);
+        let x1 = -b - delta.sqrt() / 2_f32 * a;
+        let x2 = -b + delta.sqrt() / 2_f32 * a;
         print!("x1 = {}, x2 = {}", x1, x2);
-    } else if delta < 0 {
+    } else if delta < 0_f32 {
         print!("No solution for now !");
     }
 }
